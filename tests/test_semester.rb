6f08e348fa0fb6e3c1bc252e47e77540a25dd32c
Kakashi Hatake <txi4909@gmail.com>
2016-02-24 17:55:57 -0500

    Update test_semester.rb

--- tests/test_semester.rb
+++ tests/test_semester.rb
@@ -5,6 +5,7 @@ class TestSemester < Minitest::Test
     
   def test_sample
     assert_equal(Semester.new.next_semesters("Fall2015",0),"Fall2015")
+    assert_equal(Semester.new.next_semesters("Fall2015",0),"Fall2050")
   end
 
 end
